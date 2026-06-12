6800ebf8098bc667cd0d3fcf5bc42525219273e7
Vitor Cannata de Carvalho <vitorcannata1@gmail.com>
2026-06-12 18:17:32 -0300

    Opções de cout adicionadas

--- projeto_pratico.cpp
+++ projeto_pratico.cpp
@@ -12,7 +12,6 @@ struct jogo{
 };
 
 int main(){
-
 	string nome_entrada;
 	getline(cin, nome_entrada);
 	
@@ -26,6 +25,27 @@ int main(){
 		int capacidade_vetor=40;
 		getline(entrada, linha); // ler o enter
 		jogo *vet_jogos = new jogo[capacidade_vetor];
+		
+		cout << "Deseja que os dados do arquivo sejam impressos?" << endl
+		<< "0 - Não" << endl << "1 - Sim" << endl;
+		int opção_impressão=0;
+		cin >> opção_impressão;
+		int início_impressão_partes, final_impressão_partes;
+		
+		if ((opção_impressão != 0) and (opção_impressão != 1)){
+			cout << "ERRO! Tente novamente." << endl;
+			return 1;
+		}
+		if (opção_impressão == 1){
+			cout << endl << "Deseja o arquivo inteiro ou somente um trecho?" << endl
+			<< "2 - Inteiro" << endl << "3 - Trecho" << endl;
+			cin >> opção_impressão;
+			if (opção_impressão == 3){
+				cout << endl << "Digite o trecho" << endl;
+				cin >> início_impressão_partes >> final_impressão_partes;
+			}
+		}
+			
 			for(int i = 0; i < num_registros; i++){
 				entrada >> vet_jogos[i].identificador;
 				entrada.ignore(2); // ignora ; e abre aspas
@@ -38,8 +58,14 @@ int main(){
 				getline(entrada, vet_jogos[i].descricao, '"');
 				getline(entrada, linha); // ler o enter
 				
-				cout << endl << vet_jogos[i].identificador << " " << vet_jogos[i].nome << " " << vet_jogos[i].ano_lancamento << " " 
+				if (opção_impressão == 2){
+					cout << endl << vet_jogos[i].identificador << " " << vet_jogos[i].nome << " " << vet_jogos[i].ano_lancamento << " " 
 					<< vet_jogos[i].plataforma << " " << vet_jogos[i].descricao << endl;
+				}
+				if ((opção_impressão == 3) and ((i+1 >= início_impressão_partes) and (i+1 <= final_impressão_partes))){
+					cout << endl << vet_jogos[i].identificador << " " << vet_jogos[i].nome << " " << vet_jogos[i].ano_lancamento << " " 
+					<< vet_jogos[i].plataforma << " " << vet_jogos[i].descricao << endl;
+				}
 					
 				if ((i == capacidade_vetor-1) and (num_registros > capacidade_vetor)){
 					jogo *temp = new jogo[capacidade_vetor+5];
@@ -51,9 +77,12 @@ int main(){
 					capacidade_vetor += 5;
 				}					
 		}
-		cout << "Capacidade final: " << capacidade_vetor << endl;
+		
+		cout << endl << "Capacidade final: " << capacidade_vetor << endl;
 		delete[] vet_jogos;
 	}
-	else cout << "Arquivo não encontrado";
-
+	else {
+		cout << "Arquivo não encontrado";
+	}
+	return 0;
 }
