c7db20acc321d24c4bf216f17ccbe4133cb5ca62
Vitor Cannata de Carvalho <vitorcannata1@gmail.com>
2026-06-12 17:55:32 -0300

    Redimensionamento de vetor adicionado

--- projeto_pratico.cpp
+++ projeto_pratico.cpp
@@ -23,14 +23,15 @@ int main(){
 		getline(entrada, linha); // ignora a primeira linha
 		int num_registros;
 		entrada >> num_registros;
+		int capacidade_vetor=40;
 		getline(entrada, linha); // ler o enter
-		jogo *vet_jogos = new jogo[num_registros];
+		jogo *vet_jogos = new jogo[capacidade_vetor];
 			for(int i = 0; i < num_registros; i++){
 				entrada >> vet_jogos[i].identificador;
 				entrada.ignore(2); // ignora ; e abre aspas
 				getline(entrada, vet_jogos[i].nome, '"');
 				entrada.ignore(1);
-				entrada >> vet_jogos[i].ano_lancamento; // Corrigido
+				entrada >> vet_jogos[i].ano_lancamento; // ta pegando errado
 				entrada.ignore(2);
 				getline(entrada, vet_jogos[i].plataforma, '"');
 				entrada.ignore(2);
@@ -39,7 +40,18 @@ int main(){
 				
 				cout << endl << vet_jogos[i].identificador << " " << vet_jogos[i].nome << " " << vet_jogos[i].ano_lancamento << " " 
 					<< vet_jogos[i].plataforma << " " << vet_jogos[i].descricao << endl;
+					
+				if ((i == capacidade_vetor-1) and (num_registros > capacidade_vetor)){
+					jogo *temp = new jogo[capacidade_vetor+5];
+					for (int i=0; i<capacidade_vetor; i++){
+						temp[i] = vet_jogos[i];
+					}
+					delete[] vet_jogos;
+					vet_jogos = temp;
+					capacidade_vetor += 5;
+				}					
 		}
+		cout << "Capacidade final: " << capacidade_vetor << endl;
 		delete[] vet_jogos;
 	}
 	else cout << "Arquivo não encontrado";
